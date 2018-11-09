5a17ac3a803a2d2b93e6b0d8d355a80da631c03d
w-vi <vilibald@wvi.cz>
2018-11-09 11:47:55 +0100

    Prepare for melpa

--- ox-wk.el
+++ ox-wk.el
@@ -1,7 +1,14 @@
 ;;; ox-wk.el --- Wiki Back-End for Org Export Engine
 
-;; Author: Vilibald  <vilibald@wvi.cz>
+
+;; Copyright (C) 2013-2018 Vilibald Wanča <vilibald@wvi.cz>
+
+;; Author: Vilibald Wanča <vilibald@wvi.cz>
 ;; Keywords: org, wp, wiki
+;; URL: https://github.com/w-vi/ox-wk.el
+;; Package-Requires: ((emacs "24.4") (org "8.3"))
+;; Keywords: Org, markdown, docs
+;; Version: 0.2
 
 ;; This file is not part of GNU Emacs.
 
@@ -21,12 +28,12 @@
 ;;; Commentary:
 
 ;; This library implements a Wiki back-end (dokuwiki and creole flavours) for
-;; Org exporter, based on `html' back-end. I have started based on Markdown
+;; Org exporter, based on `html' back-end.  I have started based on Markdown
 ;; backend and it deserves to be mentioned here for this reason.
 ;;
 ;; It provides two commands for export, depending on the desired
-;; output: `org-wk-export-as-wiki' (temporary buffer) and
-;; `org-wk-export-to-wiki' ("txt" file).
+;; output: `ox-wk-export-as-wiki' (temporary buffer) and
+;; `ox-wk-export-to-wiki' ("txt" file).
 
 ;;; Code:
 
@@ -42,7 +49,7 @@
   :version "24.3"
   :package-version '(Org . "8.3"))
 
-(defcustom org-wk-style 'doku
+(defcustom ox-wk-style 'doku
   "Style used to format different elements to different wiki markups.
 This variable can be set to either `doku' or `creole' at the moment."
   :group 'org-export-wk
@@ -50,8 +57,9 @@ This variable can be set to either `doku' or `creole' at the moment."
           (const :tag "Use \"Dokuwiki\" style" doku)
           (const :tag "Use \"Wiki Creole\" style" creole)))
 
-(defcustom org-wk-org-verbatim 'monospace
-  "Style used to format = and ~ markups in org file. I haven't figured out yet how to distinguish these but prefer to use monospace.
+(defcustom ox-wk-org-verbatim 'monospace
+  "Style used to format = and ~ markups in org file.
+I haven't figured out yet how to distinguish these but prefer to use monospace.
 This variable can be set to either `monospace' or `verbatim'."
   :group 'org-export-wk
   :type '(choice
@@ -64,39 +72,39 @@ This variable can be set to either `monospace' or `verbatim'."
   :menu-entry
   '(?w "Export to Wiki"
        ((?W "To temporary buffer"
-            (lambda (a s v b) (org-wk-export-as-wiki a s v)))
-        (?w "To file" (lambda (a s v b) (org-wk-export-to-wiki a s v)))
+            (lambda (a s v b) (ox-wk-export-as-wiki a s v)))
+        (?w "To file" (lambda (a s v b) (ox-wk-export-to-wiki a s v)))
         (?o "To file and open"
             (lambda (a s v b)
-              (if a (org-wk-export-to-wiki t s v)
-                (org-open-file (org-wk-export-to-wiki nil s v)))))))
-  :translate-alist '((bold . org-wk-bold)
-                     (code . org-wk-code)
-                     (src-block . org-wk-src-block)
+              (if a (ox-wk-export-to-wiki t s v)
+                (org-open-file (ox-wk-export-to-wiki nil s v)))))))
+  :translate-alist '((bold . ox-wk-bold)
+                     (code . ox-wk-code)
+                     (src-block . ox-wk-src-block)
                      (comment . (lambda (&rest args) ""))
                      (comment-block . (lambda (&rest args) ""))
-                     (example-block . org-wk-src-block)
-                     (fixed-width . org-wk-fixed-width)
+                     (example-block . ox-wk-src-block)
+                     (fixed-width . ox-wk-fixed-width)
                      (footnote-definition . ignore)
                      (footnote-reference . ignore)
-                     (headline . org-wk-headline)
-                     (horizontal-rule . org-wk-horizontal-rule)
-                     (inline-src-block . org-wk-code)
-                     (italic . org-wk-italic)
-                     (underline . org-wk-underline)
-                     (item . org-wk-item)
-                     (line-break . org-wk-line-break)
-                     (link . org-wk-link)
-                     (table . org-wk-table)
-                     (table-cell . org-wk-table-cell)
-                     (table-row . org-wk-table-row)
-                     (paragraph . org-wk-paragraph)
-                     (plain-list . org-wk-plain-list)
-                     (plain-text . org-wk-plain-text)
-                     (quote-block . org-wk-quote-block)
-                     (section . org-wk-section)
-                     (template . org-wk-template)
-                     (verbatim . org-wk-verbatim)
+                     (headline . ox-wk-headline)
+                     (horizontal-rule . ox-wk-horizontal-rule)
+                     (inline-src-block . ox-wk-code)
+                     (italic . ox-wk-italic)
+                     (underline . ox-wk-underline)
+                     (item . ox-wk-item)
+                     (line-break . ox-wk-line-break)
+                     (link . ox-wk-link)
+                     (table . ox-wk-table)
+                     (table-cell . ox-wk-table-cell)
+                     (table-row . ox-wk-table-row)
+                     (paragraph . ox-wk-paragraph)
+                     (plain-list . ox-wk-plain-list)
+                     (plain-text . ox-wk-plain-text)
+                     (quote-block . ox-wk-quote-block)
+                     (section . ox-wk-section)
+                     (template . ox-wk-template)
+                     (verbatim . ox-wk-verbatim)
                      ))
 
 ;;; Filters
@@ -105,16 +113,17 @@ This variable can be set to either `monospace' or `verbatim'."
 
 ;;; Creole Functions
 
-(defun org-wk--creole-nowiki (object contents info &optional newline)
-  "Creole has a limited set of markup very often we
-leave it as it is and go to preformatted nowiki style. OBJECT is not used atm,
-it just formats the CONTENTS. NEWLINE indicates if the markup
-should be on separate lines."
+(defun ox-wk--creole-nowiki (object contents info &optional newline)
+  "Creole has a limited set of markup so we go to preformatted nowiki style.
+Very often we leave it as it is and go to preformatted nowiki
+style.  OBJECT is not used atm, it just formats the
+CONTENTS.  NEWLINE indicates if the markup should be on separate
+lines.  INFO is a plist used as a communication channel."
   (concat "{{{" (when newline "\n") contents "}}}"))
 
 ;;;; Bold
 
-(defun org-wk-bold (bold contents info)
+(defun ox-wk-bold (bold contents info)
   "Transcode BOLD object.
 CONTENTS is the text within bold markup.  INFO is a plist used as
 a communication channel."
@@ -122,7 +131,7 @@ a communication channel."
 
 ;;;; Underline
 
-(defun org-wk-underline (underline contents info)
+(defun ox-wk-underline (underline contents info)
   "Transcode UNDERLINE object.
 CONTENTS is the text within underline markup.  INFO is a plist used as
 a communication channel."
@@ -130,52 +139,52 @@ a communication channel."
 
 ;;;; Fixed width
 
-(defun org-wk-fixed-width (fixed-width contents info)
+(defun ox-wk-fixed-width (fixed-width contents info)
   "Transcode FIXED-WIDTH element.
 CONTENTS is nil.  INFO is a plist used as a communication
 channel."
   (let ((value (org-element-property :value fixed-width)))
     (cond
-     ((eq org-wk-style 'creole) (org-wk--creole-nowiki fixed-width value info))
+     ((eq ox-wk-style 'creole) (ox-wk--creole-nowiki fixed-width value info))
      (t (format "''%s''" value)))))
 
 ;;;; Code and Verbatim
 
-(defun org-wk-code (code contents info)
+(defun ox-wk-code (code contents info)
   "Transcode CODE object.
 CONTENTS is nil.  INFO is a plist used as a communication
 channel."
   (let ((value (org-element-property :value code))
         (lang (org-element-property :language code)))
     (cond
-     ((eq org-wk-style 'creole) (org-wk--creole-nowiki code value info))
+     ((eq ox-wk-style 'creole) (ox-wk--creole-nowiki code value info))
      (t (concat "<code" (if lang (format " %s> " lang) "> ") (format "%s</code>" value))))))
 
-(defun org-wk-verbatim (verbatim contents info)
+(defun ox-wk-verbatim (verbatim contents info)
   "Transcode VERBATIM object.
 CONTENTS is nil.  INFO is a plist used as a communication
 channel."
   (let ((value (org-element-property :value verbatim)))
     (cond
-     ((eq org-wk-style 'creole) (org-wk--creole-nowiki verbatim value info))
-     ((eq org-wk-org-verbatim 'monospace)(org-wk-fixed-width verbatim contents info))
+     ((eq ox-wk-style 'creole) (ox-wk--creole-nowiki verbatim value info))
+     ((eq ox-wk-org-verbatim 'monospace)(ox-wk-fixed-width verbatim contents info))
      (t (format "%%%% %s %%%%" value)))))
 
 ;;;; Src-Block
 
-(defun org-wk-src-block (src-block contents info)
+(defun ox-wk-src-block (src-block contents info)
   "Transcode SRC-BLOCK element.
 CONTENTS is nil.  INFO is a plist used as a communication
 channel."
   (let ((lang (org-element-property :language src-block))
         (content (org-export-format-code-default src-block info)))
     (cond
-     ((eq org-wk-style 'creole) (org-wk--creole-nowiki src-block content info t))
+     ((eq ox-wk-style 'creole) (ox-wk--creole-nowiki src-block content info t))
      (t (concat "<code" (if lang (format " %s>\n" lang) ">\n") (format "%s</code>" content))))))
 
 ;;;; Headline
 
-(defun org-wk-headline (headline contents info)
+(defun ox-wk-headline (headline contents info)
   "Transcode HEADLINE element.
 CONTENTS is the headline contents.  INFO is a plist used as
 a communication channel."
@@ -201,28 +210,28 @@ a communication channel."
        ;; Cannot create a headline.  Fall-back to a list.
        ((or (and (org-export-low-level-p headline info)
                  (> (org-export-low-level-p headline info) 3))
-            (and (eq org-wk-style 'doku) (> level 5))
-            (and (eq org-wk-style 'creole) (> level 6)))
+            (and (eq ox-wk-style 'doku) (> level 5))
+            (and (eq ox-wk-style 'creole) (> level 6)))
         (let ((bullet
                (if (not (org-export-numbered-headline-p headline info)) "*" "-" )))
           (concat "  " bullet heading tags "\n\n"
                   (and contents (replace-regexp-in-string "^" "    " contents)))))
-       ((eq org-wk-style 'creole)
+       ((eq ox-wk-style 'creole)
         (concat (make-string level ?=) " " heading tags "\n\n" contents))
        (t (let ((markup (make-string (- 7 level) ?=)))
             (concat markup " " heading tags " " markup "\n\n" contents)))))))
 
 ;;;; Horizontal Rule
 
-(defun org-wk-horizontal-rule (horizontal-rule contents info)
+(defun ox-wk-horizontal-rule (horizontal-rule contents info)
   "Transcode HORIZONTAL-RULE element.
-CONTENTS is the horizontal rule contents, none is actually used.  INFO is a plist used
-as a communication channel."
+CONTENTS is the horizontal rule contents, none is actually used.
+INFO is a plist used as a communication channel."
   "----")
 
 ;;;; Italic
 
-(defun org-wk-italic (italic contents info)
+(defun ox-wk-italic (italic contents info)
   "Transcode ITALIC object.
 CONTENTS is the text within italic markup.  INFO is a plist used
 as a communication channel."
@@ -230,13 +239,13 @@ as a communication channel."
 
 ;;;; Item
 
-(defun org-wk-item (item contents info)
+(defun ox-wk-item (item contents info)
   "Transcode ITEM element.
 CONTENTS is the item contents.  INFO is a plist used as
 a communication channel."
   (let* ((plain-list (org-export-get-parent item))
          (type (org-element-property :type plain-list))
-         (bullet (if (eq org-wk-style 'creole)
+         (bullet (if (eq ox-wk-style 'creole)
                      (if (eq type 'ordered) "#" "*" )
                    (if (eq type 'ordered) "-" "*" )))
          (counter (org-element-property :counter item))
@@ -253,9 +262,9 @@ a communication channel."
               (when (eq (org-element-type parent) 'plain-list)
                 (incf level)))
             level))
-         (prefix (if (eq org-wk-style 'creole) (if (eq type 'ordered)?# ?*) ? )))
+         (prefix (if (eq ox-wk-style 'creole) (if (eq type 'ordered)?# ?*) ? )))
     (concat
-     (if (eq org-wk-style 'doku) (make-string (* 2 level) prefix )
+     (if (eq ox-wk-style 'doku) (make-string (* 2 level) prefix )
        (make-string (1- level) prefix))
      bullet " "
      (case checkbox
@@ -267,7 +276,7 @@ a communication channel."
 
 ;;;; Line Break
 
-(defun org-wk-line-break (line-break contents info)
+(defun ox-wk-line-break (line-break contents info)
   "Transcode LINE-BREAK object.
 CONTENTS is nil.  INFO is a plist used as a communication
 channel."
@@ -275,7 +284,7 @@ channel."
 
 ;;;; Link
 
-(defun org-wk-link (link contents info)
+(defun ox-wk-link (link contents info)
   "Transcode a LINK object from Org to HTML.
 
 CONTENTS is the description part of the link, or the empty string.
@@ -352,7 +361,7 @@ INFO is a plist holding contextual information.  See `org-export-data'."
 
 ;;;; Paragraph
 
-(defun org-wk-paragraph (paragraph contents info)
+(defun ox-wk-paragraph (paragraph contents info)
   "Transcode PARAGRAPH element.
 CONTENTS is the paragraph contents.  INFO is a plist used as
 a communication channel."
@@ -364,7 +373,7 @@ a communication channel."
 
 ;;;; Plain List
 
-(defun org-wk-plain-list (plain-list contents info)
+(defun ox-wk-plain-list (plain-list contents info)
   "Transcode PLAIN-LIST element.
 CONTENTS is the plain-list contents.  INFO is a plist used as
 a communication channel."
@@ -372,7 +381,7 @@ a communication channel."
 
 ;;;; Plain Text
 
-(defun org-wk-plain-text (text info)
+(defun ox-wk-plain-text (text info)
   "Transcode a TEXT string.
 TEXT is the string to transcode.  INFO is a plist holding
 contextual information."
@@ -380,7 +389,7 @@ contextual information."
     (setq text (org-export-activate-smart-quotes text :html info)))
   ;; Protect ambiguous #.  This will protect # at the beginning of
   ;; a line, but not at the beginning of a paragraph.  See
-  ;; `org-wk-paragraph'.
+  ;; `ox-wk-paragraph'.
   (setq text (replace-regexp-in-string "\n#" "\n\\\\#" text))
   ;; Protect ambiguous !
   (setq text (replace-regexp-in-string "\\(!\\)\\[" "\\\\!" text nil nil 1))
@@ -397,7 +406,7 @@ contextual information."
 
 ;;;; Quote Block
 
-(defun org-wk-quote-block (quote-block contents info)
+(defun ox-wk-quote-block (quote-block contents info)
   "Transcode QUOTE-BLOCK element.
 CONTENTS is the quote-block contents.  INFO is a plist used as
 a communication channel."
@@ -407,7 +416,7 @@ a communication channel."
 
 ;;;; Section
 
-(defun org-wk-section (section contents info)
+(defun ox-wk-section (section contents info)
   "Transcode SECTION element.
 CONTENTS is the section contents.  INFO is a plist used as
 a communication channel."
@@ -415,7 +424,7 @@ a communication channel."
 
 ;;;; Template
 
-(defun org-wk-template (contents info)
+(defun ox-wk-template (contents info)
   "Return complete document string after conversion.
 CONTENTS is the transcoded contents string.  INFO is a plist used
 as a communication channel."
@@ -423,18 +432,18 @@ as a communication channel."
 
 ;;;; Table
 
-(defun org-wk-table (table contents info)
+(defun ox-wk-table (table contents info)
   "Transcode TABLE element.
 CONTENTS is the table contents.  INFO is a plist used
 as a communication channel."
   contents)
 
-(defun org-wk-table-row  (table-row contents info)
+(defun ox-wk-table-row  (table-row contents info)
   "Transcode TABLE-ROW element.
 CONTENTS is the row contents.  INFO is a plist used
 as a communication channel."
   (cond
-   ((eq org-wk-style 'creole)
+   ((eq ox-wk-style 'creole)
     (concat
      (if (org-string-nw-p contents) (format "|%s" contents) "")))
    (t (concat
@@ -443,18 +452,18 @@ as a communication channel."
        (when (org-export-table-row-ends-header-p table-row info)
          "^")))))
 
-(defun org-wk-table-cell  (table-cell contents info)
+(defun ox-wk-table-cell  (table-cell contents info)
   "Transcode TABLE-CELL element.
 CONTENTS is the table-cell contents.  INFO is a plist used
-as a communication channel. Treat Header cells differently.
+as a communication channel.  Treat Header cells differently.
 FIXME : support also row header cells, now headers are in columns only"
   (let ((table-row (org-export-get-parent table-cell)))
     (cond
      ((org-export-table-row-starts-header-p table-row info)
-      (if (eq org-wk-style 'doku)(concat "^ " contents)
+      (if (eq ox-wk-style 'doku)(concat "^ " contents)
         (format "=%s|" contents)))
      ((org-export-table-cell-starts-colgroup-p table-cell info)
-      (if (eq org-wk-style 'doku) (concat "|" contents "|")
+      (if (eq ox-wk-style 'doku) (concat "|" contents "|")
         (format "%s|" contents)))
      (t (concat contents "|")))))
 
@@ -462,7 +471,7 @@ FIXME : support also row header cells, now headers are in columns only"
 
 ;;;###autoload
 
-(defun org-wk-export-as-wiki (&optional async subtreep visible-only)
+(defun ox-wk-export-as-wiki (&optional async subtreep visible-only)
   "Export current buffer to a Wiki buffer.
 
 If narrowing is active in the current buffer, only export its
@@ -502,16 +511,16 @@ non-nil."
         (switch-to-buffer-other-window outbuf)))))
 
 ;;;###autoload
-(defun org-wk-convert-region-to-wk ()
-  "Assume the current region has org-mode syntax, and convert it to Wiki syntax.
+(defun ox-wk-convert-region-to-wk ()
+  "Assume current region has ‘org-mode’ syntax and convert it to Wiki syntax.
 This can be used in any buffer.  For example, you can write an
-itemized list in org-mode syntax in a Wiki sytntax buffer and use
+itemized list in ‘org-mode’ syntax in a Wiki sytntax buffer and use
 this command to convert it."
   (interactive)
   (org-export-replace-region-by 'wk))
 
 ;;;###autoload
-(defun org-wk-export-to-wiki (&optional async subtreep visible-only)
+(defun ox-wk-export-to-wiki (&optional async subtreep visible-only)
   "Export current buffer to a Wiki sytntax text file.
 
 If narrowing is active in the current buffer, only export its
